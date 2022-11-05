eeac56ac489cabb5f9586a5b5fdb5f7286625d53
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-05 11:17:50 -0400

    Add script to write book json data

---
+++ src/writeJSON.js
@@ -0,0 +1,46 @@
+const fs = require("fs");
+const path = require("path");
+
+function writeJSON() {
+  const contents = fs.readFileSync(
+    path.resolve(__dirname, `./assets/book-club.csv`),
+    "utf-8"
+  );
+  const contentsArr = contents.split(/\r\n/);
+  const headers = contentsArr.splice(0, 1)[0].split(",");
+
+  const books = contentsArr.map((row) => {
+    const items = row.split(",");
+    const book = {};
+    headers.forEach((header, i) => {
+      book[`${header}`] = items[i];
+    });
+    return {
+      title: book.title,
+      author: book.author,
+      image: `./img/${book.image}.jpg`,
+      chosenBy: book.chosenBy,
+      meetDate: book.meetDate,
+      stats: {
+        publishYear: book.publishYear,
+        audioHours: +book.audioHours,
+        pageCount: +book.pageCount,
+        wordCount: +book.wordCount,
+        fiction: book.fiction === "true" ? true : false,
+      },
+      ratings: {
+        connor: +book.connor || null,
+        joe: +book.joe || null,
+        kris: +book.kris || null,
+        rusty: +book.rusty || null,
+        avg: +book.avg || null,
+      },
+    };
+  });
+  fs.writeFileSync(
+    path.resolve(__dirname, `./assets/books.json`),
+    JSON.stringify(books)
+  );
+}
+
+writeJSON();
