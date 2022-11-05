ac2be0e63b97773f94d34875a8aad3df784b2a59
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-05 14:17:54 -0400

    Render book list, add initial modal functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import "./App.css";
+import { useState } from "react";
+import { users } from "./assets/users";
+import books from "./assets/books.json";
+import BookList from "./components/BookList";
+import Modal from "./components/Modal";
+import "./sass/main.scss";
 
 function App() {
-  return <div className="App"></div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalBook, setModalBook] = useState({});
+
+  const openModal = (book) => {
+    setIsModalOpen(true);
+    setModalBook(book);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <>
+      {isModalOpen && <Modal book={modalBook} closeModal={closeModal} />}
+      <main className="container">
+        <h1>Book Club</h1>
+        <BookList openModal={openModal} books={books} />
+      </main>
+    </>
+  );
 }
 
 export default App;
