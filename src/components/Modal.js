4a397164940a418b2449851114e7f34b3ffcdb4f
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-06 22:28:58 -0500

    Add and style modal icons and stats

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,19 +1,77 @@
 import React from "react";
+import {
+  FaMusic,
+  FaBookOpen,
+  FaRegCalendar,
+  FaSlackHash,
+} from "react-icons/fa";
 
 const Modal = (props) => {
-  const { title, author, ratings } = props.book;
+  const { title, author, ratings, image } = props.book;
+  const { audioHours, wordCount, pageCount, publishYear } = props.book.stats;
   return (
-    <div className="modal">
-      <h2>{title}</h2>
-      <h3>{author}</h3>
-      <div class="ratings">
-        <p>Connor: {ratings.connor || "n/a"}</p>
-        <p>Joe: {ratings.joe || "n/a"}</p>
-        <p>Kris: {ratings.kris || "n/a"}</p>
-        <p>Rusty: {ratings.rusty || "n/a"}</p>
+    <>
+      <div class="overlay" onClick={props.closeModal}></div>
+      <div className="modal">
+        <button className="btn modal__btn-close" onClick={props.closeModal}>
+          &times;
+        </button>
+        <img src={`./img/${image}`} alt={title} />
+        <div className="modal__body">
+          <header>
+            <h2 className="modal__title">{title}</h2>
+            <h3 className="modal__author">by {author}</h3>
+          </header>
+          <div className="modal__info">
+            <p className="modal__stat">
+              <FaRegCalendar className="modal__icon" />
+              <span>
+                Published in <strong>{publishYear}</strong>
+              </span>
+            </p>
+            <p className="modal__stat">
+              <span className="modal__letter-icon">C</span>
+              {ratings.connor || "n/a"}
+            </p>
+
+            <p className="modal__stat">
+              <FaMusic className="modal__icon" />
+              <span>
+                <strong>{Math.round(audioHours * 10) / 10}</strong> hrs
+              </span>
+            </p>
+            <p className="modal__stat">
+              <span className="modal__letter-icon">J</span>
+              {ratings.joe || "n/a"}
+            </p>
+
+            <p className="modal__stat">
+              <FaSlackHash className="modal__icon" />
+              <span>
+                <strong>{wordCount.toLocaleString()}</strong> words
+              </span>
+            </p>
+            <p className="modal__stat">
+              <span className="modal__letter-icon">K</span>
+              {ratings.kris || "n/a"}
+            </p>
+
+            <p className="modal__stat">
+              <FaBookOpen className="modal__icon" />
+              <span>
+                <strong>{pageCount.toLocaleString()}</strong> pages
+              </span>
+            </p>
+            <p className="modal__stat">
+              <span className="modal__letter-icon">R</span>
+              {ratings.rusty || "n/a"}
+            </p>
+
+            {/* <div className="modal__ratings"></div> */}
+          </div>
+        </div>
       </div>
-      <button onClick={props.closeModal}>CLOSE</button>
-    </div>
+    </>
   );
 };
 
