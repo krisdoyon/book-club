729607d0ff99a78c688a50a3f15639f2bb7615d6
Kris Doyon <kristopher.doyon@gmail.com>
2022-11-14 14:10:31 -0500

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,28 @@
-import { useState } from "react";
-// DATA
-import { members } from "./assets/members";
-import bookData from "./assets/books.json";
+// ROUTER
+import { Routes, Route } from "react-router-dom";
 // COMPONENTS
 import Modal from "./components/Modal";
-import Header from "./components/Header";
 import Books from "./pages/books/Books";
 import Stats from "./pages/stats/Stats";
-import Footer from "./components/Footer";
+import Layout from "./pages/Layout";
+// CONTEXT
+import { useGlobalContext } from "./context/context";
 // SASS
 import "./sass/main.scss";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalBook, setModalBook] = useState({});
-  const [page, setPage] = useState("books");
-
-  const openModal = (book) => {
-    setIsModalOpen(true);
-    setModalBook(book);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
+  const { isModalOpen } = useGlobalContext();
   return (
     <>
-      {isModalOpen && <Modal book={modalBook} closeModal={closeModal} />}
-      <Header page={page} setPage={setPage} />
-      <main>
-        {page === "books" && (
-          <Books bookData={bookData} openModal={openModal} />
-        )}
-        {page === "stats" && <Stats bookData={bookData} members={members} />}
-      </main>
-      <Footer />
+      {isModalOpen && <Modal />}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Books />} />
+          <Route path="stats" element={<Stats />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+// REACT
 import React from "react";
 import ReactDOM from "react-dom/client";
+// ROUTER
+import { BrowserRouter as Router } from "react-router-dom";
+// CONTEXT PROVIDER
+import { AppProvider } from "./context/context";
+// COMPONENTS
+import ScrollToTop from "./helpers/ScrollToTop";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <AppProvider>
+        <ScrollToTop />
+        <App />
+      </AppProvider>
+    </Router>
   </React.StrictMode>
 );
