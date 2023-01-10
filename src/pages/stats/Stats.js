a0ac37b8436b8b40aea7ab7488f8a9673b504b8a
Kris Doyon <kristopher.doyon@gmail.com>
2023-01-09 21:29:27 -0500

    Add 2023 to year filter

--- src/pages/stats/Stats.js
+++ src/pages/stats/Stats.js
@@ -81,6 +81,7 @@ const Stats = () => {
             }
           >
             <option value="all">All</option>
+            <option value="2023">2023</option>
             <option value="2022">2022</option>
             <option value="2021">2021</option>
             <option value="2020">2020</option>
